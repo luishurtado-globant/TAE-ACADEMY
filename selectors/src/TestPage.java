b7d9d260263e029a4282166af25ae62e9f96f71a
luishurtado-globant <97626965+luishurtado-globant@users.noreply.github.com>
2022-04-20 14:48:58 -0500

    Selectors/locators homework

---
+++ selectors/src/TestPage.java
@@ -0,0 +1,12 @@
+public class TestPage {
+    String authorName = "By.css .container address:first-of-type a:first-child ";
+    String liElement = "By.css .container ul li:nth-child(3)";
+    String secondList = "By.css .container menu :first-child";
+    String thirdList = "By.css .container dir :last-of-type";
+    String buttonForm = "By.css .container form div:nth-child(2) button";
+    String resetButtonForm = "By.className btn reset2";
+    String selectOption = "By.css #f11 :nth-child(3)";
+    String tableContent = "By css tr:nth-of-type(4) td[align=\"RIGHT\"]:nth-child(3)";
+    String paragraph = "By css .container p:nth-of-type(7)";
+    String subtitle = "By css h3:nth-of-type(2)";
+}
